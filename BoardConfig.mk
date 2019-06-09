de450a6c19e18c09a70a4a5568ffce59e24c9009
Artem Borisov <dedsa2002@gmail.com>
2019-06-09 13:49:58 +0530

    mido: Update vendor SPL
    Change-Id: I19ecf5596728e41c89301424eefbc9907a4c1d35
    Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,7 +193,7 @@ DISABLE_RILD_OEM_HOOK := true
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-04-01
+VENDOR_SECURITY_PATCH := 2019-05-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
